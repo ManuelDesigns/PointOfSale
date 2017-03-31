a3783527045a3f13cd834b6204b737f06f64e009
Unknown <mpaniagua7@mail.csuchico.edu>
2017-03-30 22:49:30 -0700

    update

--- sales.js
+++ sales.js
@@ -4,18 +4,18 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
+  if (isNaN(newItem)) {
+    alert("Enter Numerical Values Only");
+  }
+  else {
+    newItem = Number(newItem);
+    runningTotal += newItem;
+    var dollars = asCurrency(runningTotal);
+    document.getElementById("subtotal").innerHTML = dollars;
+    document.getElementById("price").value = "";
+    setCookie("preTax", runningTotal, 30);
+  }
 }
-
 //takes a number and gives a string with the number displayed as USD currency
 function asCurrency(val)
 {
@@ -43,4 +43,13 @@ function getCookie(cname) {
         }
     }
     return "";
+}
+
+function calculateReceipt() {
+  var receiptSubtotal = Number(getCookie("preTax"));
+  var receiptTax = receiptSubtotal*.075;
+  var receiptTotal = asCurrency(receiptTax+receiptSubtotal);
+  document.getElementById("sub").innerHTML = asCurrency(receiptSubtotal);
+  document.getElementById("tax").innerHTML = asCurrency(receiptTax);
+  document.getElementById("tot").innerHTML = receiptTotal;
 }
